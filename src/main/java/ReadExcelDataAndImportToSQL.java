f920344ebfdbd15bfe29a7bf1a5cc1de41eccb4a
Pavan Koushik <bkoushik@deloitte.com>
2022-10-17 22:41:37 +0530

    Modified changes w.r.t sequence generator and insert statements

--- src/main/java/ReadExcelDataAndImportToSQL.java
+++ src/main/java/ReadExcelDataAndImportToSQL.java
@@ -2,10 +2,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
@@ -28,7 +26,10 @@ public class ReadExcelDataAndImportToSQL
         {
             // Give the Absolute Path of your file here
             // Please verify the sql files are generated in resources module.
-            File file = new File("src/main/resources/Details.xlsx");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the file name: ");
+            String filename = sc.next();
+            File file = new File("src/main/resources/"+filename+".xlsx");
             String fileName = file.getName().split("\\.")[0];
             FileInputStream inputStream = new FileInputStream(file);
             XSSFWorkbook wb=new XSSFWorkbook(inputStream);
@@ -91,20 +92,36 @@ public class ReadExcelDataAndImportToSQL
             e.printStackTrace();
         }
     }
+    static String sequenceStr ="";
+    static String sequenceName ="";
     private static void createQuery(List<String> rowHeaderFromExcel, String fileName)
     {
+        // create a sequence:
+        sequenceName = fileName+"_id_seq";
+        sequenceStr = "CREATE SEQUENCE "+ sequenceName +" start with 1 increment by 1 nomaxvalue;";
+        System.out.println("Sequence : "+sequenceStr);
+        Path path= Paths.get("src/main/resources/Sequence.sql");
+        try{
+            Files.writeString(path, sequenceStr, StandardCharsets.UTF_8);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
         StringBuilder sb = new StringBuilder();
         sb.append("Create Table "+fileName + " ( ");
-        sb.append(fileName+"_id serial PRIMARY KEY, ");
+        sb.append(fileName+"_id INT PRIMARY KEY , ");
         for(int i = 0 ; i <rowHeaderFromExcel.size() ;i++){
             sb.append(rowHeaderFromExcel.get(i)+" VARCHAR(200) NOT NULL,");
         }
         String createQuery = sb.substring(0, sb.length()-1)+");";
         System.out.println(createQuery);
 
-        Path path= Paths.get("src/main/resources/Create.sql");
+        Path path1= Paths.get("src/main/resources/Create.sql");
         try{
-            Files.writeString(path, createQuery, StandardCharsets.UTF_8);
+            Files.writeString(path1, createQuery, StandardCharsets.UTF_8);
         }
         catch (Exception e)
         {
@@ -117,7 +134,7 @@ public class ReadExcelDataAndImportToSQL
     private static void createInsertQueries(List<List<String>> rowBodyFromExcel, String fileName)
     {
         StringBuilder insertQueryBuilder = new StringBuilder();
-        insertQueryBuilder.append("INSERT INTO "+fileName+ " ( ");
+        insertQueryBuilder.append("INSERT INTO "+fileName+ " ( "+ fileName+"_id ," );
         List<String> rowHeader = rowBodyFromExcel.get(0);
         for(int i = 0; i < rowHeader.size(); i++){
             insertQueryBuilder.append(" " + rowHeader.get(i)+" ,");
@@ -127,27 +144,29 @@ public class ReadExcelDataAndImportToSQL
 //        System.out.println("-->"+insertQuery);
 
 
+        String nextValInQuery = "nextVal(\'"+sequenceName+"\' ,)";
         List<String> insertQueries = new ArrayList<>();
 
         for(int i = 1; i < rowBodyFromExcel.size(); i++)
         {
             StringBuilder valuesBuilder = new StringBuilder();
             List<String> rowCell = rowBodyFromExcel.get(i);
+            valuesBuilder.append(nextValInQuery);
             for(int j=0; j < rowCell.size(); j++ )
             {
-                valuesBuilder.append(" \""+rowBodyFromExcel.get(i).get(j)+"\" ,");
+                valuesBuilder.append(" \'"+rowBodyFromExcel.get(i).get(j)+"\' ,");
             }
             //System.out.println(insertQuery + valuesBuilder.substring(0,valuesBuilder.length()-1)+");") ;
             insertQueries.add(insertQuery + valuesBuilder.substring(0,valuesBuilder.length()-1)+");");
         }
-        System.out.println("List of insert Queries---\n "+ insertQueries);
+        //System.out.println("List of insert Queries---\n "+ insertQueries);
 
         String finalInsertQueries = "";
         for(int i  = 0; i < insertQueries.size() ; i++){
             finalInsertQueries = finalInsertQueries + insertQueries.get(i)+"\n";
         }
 
-        System.out.println("List of insert Queries---"+ finalInsertQueries.substring(0, finalInsertQueries.length()-1));
+        System.out.println("List of insert Queries---\n "+ finalInsertQueries.substring(0, finalInsertQueries.length()-1));
 
         // Save the queries in the insert.sql file
         Path path= Paths.get("src/main/resources/Insert.sql");
--- src/main/resources/Insert.sql
+++ src/main/resources/Insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO Details (  Name , Number , City , State )  VALUES ( "Test 1" , "1234.0" , "Hyderabad" , "Telangana" );
-INSERT INTO Details (  Name , Number , City , State )  VALUES ( "Test 2" , "2345.0" , "Vijayawada" , "Andhra Pradesh" );
-INSERT INTO Details (  Name , Number , City , State )  VALUES ( "Test 3" , "1.2344555E7" , "Mumbai" , "Maharashtra" );
-INSERT INTO Details (  Name , Number , City , State )  VALUES ( "Test 4" , "8900.0" , "Bangalore" , "Karnataka" );
-INSERT INTO Details (  Name , Number , City , State )  VALUES ( "Test 5" , "1000.0" , "Chennai" , "Tamilnadu" );
-INSERT INTO Details (  Name , Number , City , State )  VALUES ( "Test 6 " , "1001.0" , "Gurgaon" , "Delhi" );
+INSERT INTO details ( details_id , Name , Number , City , State )  VALUES (nextVal('details_id_seq' ,) 'Test 1' , '1234.0' , 'Hyderabad' , 'Telangana' );
+INSERT INTO details ( details_id , Name , Number , City , State )  VALUES (nextVal('details_id_seq' ,) 'Test 2' , '2345.0' , 'Vijayawada' , 'Andhra Pradesh' );
+INSERT INTO details ( details_id , Name , Number , City , State )  VALUES (nextVal('details_id_seq' ,) 'Test 3' , '1.2344555E7' , 'Mumbai' , 'Maharashtra' );
+INSERT INTO details ( details_id , Name , Number , City , State )  VALUES (nextVal('details_id_seq' ,) 'Test 4' , '8900.0' , 'Bangalore' , 'Karnataka' );
+INSERT INTO details ( details_id , Name , Number , City , State )  VALUES (nextVal('details_id_seq' ,) 'Test 5' , '1000.0' , 'Chennai' , 'Tamilnadu' );
+INSERT INTO details ( details_id , Name , Number , City , State )  VALUES (nextVal('details_id_seq' ,) 'Test 6 ' , '1001.0' , 'Gurgaon' , 'Delhi' );
